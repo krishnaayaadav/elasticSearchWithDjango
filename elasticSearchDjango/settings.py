4514ff8f8afa537aae511035ecd5d99e2a20489b
Krishna Yadav <krishnayaadav7431@gmail.com>
2024-07-12 22:06:27 +0530

    configured PostgreSQL database is configured

--- elasticSearchDjango/settings.py
+++ elasticSearchDjango/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'elasticSearchDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_search_db',
+        'HOST': 'localhost',
+        'USER': 'krish',
+        'PASSWORD': 'krishna123',
+        'PORT': '5432'
     }
 }
 
