e65e40085457f4d6eab0fe67eefebb7a5c5ed396
Krishna Yadav <krishnayaadav7431@gmail.com>
2024-07-12 23:53:30 +0530

    modified Blog search modified multi-match field added to filter blog using multiple fields

--- apps/blogs_app/views.py
+++ apps/blogs_app/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
+from elasticsearch_dsl import Q
 from django.views import View
 from apps.blogs_app.documents import BlogModelDocument
 
+from django.core.paginator import PageNotAnInteger,EmptyPage, Paginator
+
 # Create your views here.
+
+# get paginated reponse
+def get_paginated_response(result_objects, page_size, request):
+    
+    page_num = request.GET.get('page')
+
+    paginator = Paginator(result_objects, page_size)
+
+    try:
+        paginated_result = paginator.page(page_num)
+    
+    except PageNotAnInteger:
+        paginated_result = paginator.page(1)
+    
+    except EmptyPage:
+        paginated_result = paginator.page(paginator.num_pages)
+    
+    finally:
+        return paginated_result
 
 
 class BlogSearchview(View):
@@ -14,9 +36,17 @@ class BlogSearchview(View):
         print(search_query)
 
         if search_query:
-            results = BlogModelDocument.search().query('match', category=search_query)
-        
+            # search with multiple-model fields here
+            q = Q(
+                'multi_match',
+                query=search_query,
+                fields=['name', 'description', 'category', 'author']
+            )
+            search_result = BlogModelDocument.search().query(q)
         else:
-            results = BlogModelDocument.search().query('match_all')
+            search_result = BlogModelDocument.search().query('match_all')
         
-        return render(request, 'blog_search.html', {'search_results': results})
+
+
+        
+        return render(request, 'blog_search.html', {'paginated_result': search_result})
